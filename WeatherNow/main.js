cfc107df9a954364326940e6a7f2cf104d3383e0
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-06-12 20:25:52 -0300

    javascript teste de api

--- WeatherNow/main.js
+++ WeatherNow/main.js
@@ -1,8 +1,22 @@
-
 const key = "e7f4f52c71e0f15967cf64c30033356c"
 
-function click_search() {
-    const input = document.querySelector(".input_search").value
+// buscando no servidor os dados e alterando //
+function  putting_screen(dados){
+    documento.querySelector(".city").innerHTML = "Tempo em" + dados.name
+}
 
-    console.log(input)
+// SEMPRE QUE FOR PEGAR OS DADOS DE UM SERVIDOR, É IMPORTANTE USAR FUNÇÕES ASSICRONAS- async //
+// 
+async function searchCity(city) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br`).then(response => response.json())
+    // CONSTANTE PARA GUARDAS DOS DADOS (AWAIT) FETCH PARA ACESSAR O SERVIDOR //
+    // json = transforma o dado recebido do servidor me JavaScript. //
+    console.log(dados);
+}
+
+// Pegando as informações do input associado ao clique do botão
+function click_search() {
+    const city = document.querySelector(".input_search").value;
+
+    searchCity(city);
 }
