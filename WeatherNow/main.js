02b04c2c94dfc5cd77b353dbbcc6a5f2a3b35fec
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-06-13 22:26:42 -0300

    captur input dados

--- WeatherNow/main.js
+++ WeatherNow/main.js
@@ -1,14 +1,16 @@
+/* ANALISAR NOVA ESTRITURA DE CÓDIGO E VER O QUE ACONTECEU ANTERIORMENTE, 
 const key = "e7f4f52c71e0f15967cf64c30033356c"
 
 // buscando no servidor os dados e alterando //
 function  putting_screen(dados){
-    documento.querySelector(".city").innerHTML = "Tempo em" + dados.name
+    document.querySelector(".city").innerHTML = "Tempo em" + dados.name;
 }
 
 // SEMPRE QUE FOR PEGAR OS DADOS DE UM SERVIDOR, É IMPORTANTE USAR FUNÇÕES ASSICRONAS- async //
 // 
 async function searchCity(city) {
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br`).then(response => response.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br`)
+    .then(response => response.json())
     // CONSTANTE PARA GUARDAS DOS DADOS (AWAIT) FETCH PARA ACESSAR O SERVIDOR //
     // json = transforma o dado recebido do servidor me JavaScript. //
     console.log(dados);
@@ -19,4 +21,26 @@ function click_search() {
     const city = document.querySelector(".input_search").value;
 
     searchCity(city);
+}
+*/
+
+
+const key = "e7f4f52c71e0f15967cf64c30033356c";
+
+function putting_screen(dados) {
+  document.querySelector(".city").innerHTML = "Tempo em " + dados.name;
+}
+
+async function searchCity(city) {
+  const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br`)
+    .then(response => response.json());
+  
+  putting_screen(dados); // Chama a função putting_screen() para atualizar o texto da classe "city" no HTML
+  
+  console.log(dados);
+}
+
+function click_search() {
+  const city = document.querySelector(".input_search").value;
+  searchCity(city);
 }
