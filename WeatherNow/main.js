db3ccf2ce80aa2c2b86f0afa9284b85ae0d170bf
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-06-15 20:57:35 -0300

    tratamento de erro.

--- WeatherNow/main.js
+++ WeatherNow/main.js
@@ -3,12 +3,12 @@ const key = "e7f4f52c71e0f15967cf64c30033356c"
 
 // buscando no servidor os dados e alterando //
 function  putting_screen(dados){
-    document.querySelector(".city").innerHTML = "Tempo em " + dados.name;
+    document.querySelector(".city").innerHTML = "Tempo em " + (dados.name || "Nome da cidade não disponível");
     document.querySelector(".temperatura").innerHTML = Math.floor(dados.main.temp) + "°C";
     document.querySelector(".text-forecast").innerHTML = dados.weather[0].description;
     document.querySelector(".humidity").innerHTML = "Umidade: " + dados.main.humidity + "%";
     document.querySelector(".img-forecast").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png` 
-    document.querySelector(".text_information").innerHTML = "O tempo na cidade de " + dados.name + " é de " + Math.floor(dados.main.temp) + "°C" + " com " + dados.weather[0].description;
+    document.querySelector(".text_information").innerHTML = "O tempo na cidade de " + dados.name + " é de " + Math.floor(dados.main.temp) + "°C" + " - " + dados.weather[0].description;
 }
 
 // SEMPRE QUE FOR PEGAR OS DADOS DE UM SERVIDOR, É IMPORTANTE USAR FUNÇÕES ASSICRONAS- async //
@@ -30,52 +30,3 @@ function click_search() {
 
     searchCity(city);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const key = "e7f4f52c71e0f15967cf64c30033356c";
-
-function putting_screen(dados) {
-  document.querySelector(".city").innerHTML = "Tempo em " + dados.name;
-  document.querySelector(".temperatura").innerHTML = dados.maiin.temp;
-}
-
-async function searchCity(city) {
-  const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br`)
-    .then(response => response.json());
-  
-  putting_screen(dados); // Chama a função putting_screen() para atualizar o texto da classe "city" no HTML
-  
-  console.log(dados);
-}
-
-function click_search() {
-  const city = document.querySelector(".input_search").value;
-  searchCity(city);
-}
-*/
